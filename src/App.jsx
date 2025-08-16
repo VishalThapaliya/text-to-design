b1b010848df5de5540b89a12891351e90f768587
Bishal Thapaliya <bishalthapaliya@Bishals-MacBook-Pro.local>
2025-08-16 19:46:00 +0200

    add header section and remove tailwind css configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Header from './components/header/Header'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='app-wrapper'>
+      <Header />
+    </div>
   )
 }
 
